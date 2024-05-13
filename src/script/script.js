f1d61ff90e81547843bde4b125e9e67c82440682
Novebrian <notvebrian@gmail.com>
2024-05-13 20:06:21 +0700

    Commit message

--- src/script/script.js
+++ src/script/script.js
@@ -4,3 +4,10 @@ const ham = document.querySelector('#ham');
 ham.addEventListener('click', function(){
     ham.classList.toggle('ham-active');
 });
+
+// Navbar Fixed
+window.onscroll = function()
+{
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+}
