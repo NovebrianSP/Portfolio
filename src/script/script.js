9454ca588249e005d802fe145fcee32654e1ef81
Novebrian <notvebrian@gmail.com>
2024-05-17 14:42:27 +0700

    Commit message

--- src/script/script.js
+++ src/script/script.js
@@ -2,20 +2,24 @@
 const ham = document.querySelector('#ham');
 const navMenu = document.querySelector('#nav-menu');
 
-ham.addEventListener('click', function(){
+ham.addEventListener('click', function () {
     ham.classList.toggle('ham-active');
     navMenu.classList.toggle('hidden');
 });
 
 // Navbar Fixed
-window.onscroll = function()
-{
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-    if(window.scrollY > fixedNav){
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
